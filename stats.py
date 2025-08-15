78a69038b03c6885128b079d1ace8ee9687478dc
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-08-15 10:45:33 -0600

    add new scored data

--- stats.py
+++ stats.py
@@ -18,21 +18,28 @@ RESULTS_DIR = "/home/drc/Downloads/uniwig_paper_figs/"
 # File paths for all four data sources
 cc_scored_data = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/scored_stats/cc_scored_data.csv"
 cc_no_scored_data = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/no_score_stats/cc_no_score_data.csv"
+cc_new_score = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/scored_stats/cc_NEW_score_data.csv"
 
 ccf_scored_data = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/scored_stats/ccf_scored_data.csv"
 ccf_no_score_data = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/no_score_stats/ccf_no_score_data.csv"
+ccf_new_score = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/scored_stats/ccf_NEW_score_data.csv"
 
 hmm_scored_data = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/scored_stats/hmm_scored_data.csv"
 hmm_no_score_data = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/no_score_stats/hmm_no_score_data.csv"
+hmm_new_score = "/home/drc/Downloads/GTARS_PAPER/PROCESSED/STATS/scored_stats/hmm_NEW_score_data.csv"
 
 # Read the data from all four CSV files
 df_cc_score = pd.read_csv(cc_scored_data)
 df_cc_no_score = pd.read_csv(cc_no_scored_data)
+df_cc_new_score = pd.read_csv(cc_new_score)
+
 df_ccf_score = pd.read_csv(ccf_scored_data)
 df_ccf_no_score = pd.read_csv(ccf_no_score_data)
+df_ccf_new_score = pd.read_csv(ccf_new_score)
 
 df_hmm_score  = pd.read_csv(hmm_scored_data)
 df_hmm_no_score  = pd.read_csv(hmm_no_score_data)
+df_hmm_new_score = pd.read_csv(hmm_new_score)
 
 # Define the column name for the statistic you want to plot
 
@@ -41,15 +48,23 @@ stats = ["median_dist_file_to_universe", 'univers/file', 'file/universe', 'unive
 # Add a 'data_source' column to each DataFrame to differentiate them
 df_cc_score['data_source'] = 'cc_scored'
 df_cc_no_score['data_source'] = 'cc_no_score'
+df_cc_new_score['data_source'] = 'cc_new_score'
+
 df_ccf_score['data_source'] = 'ccf_scored'
 df_ccf_no_score['data_source'] = 'ccf_no_score'
+df_ccf_new_score['data_source'] = 'ccf_new_score'
+
 df_hmm_score['data_source'] = 'hmm_scored'
 df_hmm_no_score['data_source'] = 'hmm_no_score'
+df_hmm_new_score['data_source'] = 'hmm_new_score'
 
-all_dfs = [df_cc_score,df_cc_no_score,df_ccf_score,df_ccf_no_score,df_hmm_score,df_hmm_no_score]
-# Concatenate all four DataFrames into a single one
-#df_combined = pd.concat([df_cc_score, df_cc_no_score, df_ccf_score, df_ccf_no_score])
-df_combined = pd.concat([df_cc_score, df_ccf_score, df_hmm_score, df_cc_no_score, df_ccf_no_score, df_hmm_no_score])
+df_combined = pd.concat([df_cc_score, df_cc_new_score, df_ccf_score,df_ccf_new_score, df_hmm_score,df_hmm_new_score, df_cc_no_score, df_ccf_no_score, df_hmm_no_score])
+
+#df_combined = pd.concat([df_ccf_score, df_ccf_new_score, df_ccf_no_score])
+#df_combined = pd.concat([df_cc_score, df_cc_new_score, df_cc_no_score])
+#df_combined = pd.concat([df_hmm_score,df_hmm_new_score,df_hmm_no_score])
+
+df_combined.dropna(inplace=True) # ccf_new has a few NaNs in median_dist column
 
 # From Reading the Paper
 # univers only - "univers/file" = FP
